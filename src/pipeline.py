b0c4d9486a759c322f01550d2824e426bd533565
JanaKatt <norambuena129@gmail.com>
2026-04-15 20:55:35 -0400

    Add files via upload

--- src/pipeline.py
+++ src/pipeline.py
@@ -1,14 +1,8 @@
-import pandas as pd
 from sklearn.pipeline import Pipeline
-from sklearn.compose import ColumnTransformer, make_column_selector
-from src.transformers import cargar_y_limpiar_pitchfork
-
 from sklearn.preprocessing import FunctionTransformer
 
 def build_pitchfork_pipeline():
 
     return Pipeline([
-        ('cleaner', FunctionTransformer(lambda X: cargar_y_limpiar_pitchfork(db_path)))
+        ('cleaner', FunctionTransformer(lambda df: df))
     ])
-
-    return pitchfork_pipe
