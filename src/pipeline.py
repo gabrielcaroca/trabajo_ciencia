f9998ed985270a398c8e03b51faf4f04bb8b8b9d
JanaKatt <norambuena129@gmail.com>
2026-04-15 20:08:13 -0400

    Daba error

--- src/pipeline.py
+++ src/pipeline.py
@@ -1,19 +1,17 @@
 import pandas as pd
 from sklearn.pipeline import Pipeline
 from sklearn.compose import ColumnTransformer, make_column_selector
-from src.transformers import PitchforkCleaner # Tu transformador personalizado
+from src.transformers import cargar_y_limpiar_pitchfork
 
 def build_pitchfork_pipeline():
     """
     Construye un Pipeline de Scikit-Learn que integra la limpieza
     y optimizacion de los datos de Pitchfork.
     """
-    
-    # En este caso, como tu limpieza es integral (afecta a todo el dataframe),
-    # el pipeline es directo. Si tuvieras modelos de ML, se añadirian aqui.
+
     
     pitchfork_pipe = Pipeline([
-        ('cleaner', PitchforkCleaner())
+        ('cleaner', cargar_y_limpiar_pitchfork())
     ])
 
     return pitchfork_pipe
